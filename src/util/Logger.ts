a3a271f768ca070c9a6aca348cf17c92805bf5ca
Enrico D'Angelo <enricodangelo@protonmail.com>
2021-11-18 11:11:39 +0000

    using winston now

--- src/util/Logger.ts
+++ src/util/Logger.ts
@@ -1,5 +1,32 @@
+import { default as winston } from 'winston';
+
 export class Logger {
+  private static logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'FrontService' },
+    transports: [
+      new winston.transports.Console(),
+    ],
+  });
+
+  static debug(msg: string): void {
+    Logger.logger.debug(msg);
+  }
+
+  static info(msg: string): void {
+    Logger.logger.info(msg);
+  }
+
+  static warn(msg: string): void {
+    Logger.logger.warn(msg);
+  }
+
+  static error(msg: string): void {
+    Logger.logger.error(msg);
+  }
+
   static log(msg: string): void {
-    console.log(`${new Date().toISOString()}: ${msg}`);
+    Logger.info(msg)
   }
 }
